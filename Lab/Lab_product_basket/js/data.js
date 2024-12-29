7c1fb8be042524071f497c76e957de82893728d3
Quang <quangquan23451822@gmail.com>
2024-12-29 21:54:52 +0700

    minhwang

---
+++ Lab/Lab_product_basket/js/data.js
@@ -0,0 +1,62 @@
+let products = [
+    {
+        product_name: 'Kệ sách',
+        product_price: 17999000,
+        product_image: 'images/1_acfd92fbcedd4b1ea2cd9d78582bb6c8_master.jpg'
+    },
+    {
+        product_name: 'Bàn bếp',
+        product_price: 19999000,
+        product_image: 'images/6-11.jpg'
+    },
+    {
+        product_name: 'Giường ngủ',
+        product_price: 24999000,
+        product_image: 'images/avar-giuong-ngu-hop.jpg'
+    },
+    {
+        product_name: 'Bàn tiếp khách',
+        product_price: 21999000,
+        product_image: 'images/ban-ghe-go-tiep-khach-van-phong.jpg'
+    },
+    {
+        product_name: 'Bàn uống nước',
+        product_price: 21799000,
+        product_image: 'images/Ban-go-uong-nuoc-thanh-lich-cho-gia-dinh-GHS-4694-ava.jpg'
+    },
+    {
+        product_name: 'Bàn học',
+        product_price: 16099000,
+        product_image: 'images/ban-hoc-dep-hien-dai-ava.png'
+    },
+    {
+        product_name: 'Bàn làm việc',
+        product_price: 17799000,
+        product_image: 'images/avar-giuong-ngu-hop.jpg'
+    },
+    {
+        product_name: 'Ghế làm việc',
+        product_price: 3099000,
+        product_image: 'images/Ghe-da-ngoi-van-phong-cao-cap-ZA7793-9.jpg'
+    },
+    {
+        product_name: 'Ghế Sofa',
+        product_price: 15999000,
+        product_image: 'images/ghe-sofa-vang-da-phong-khach-sfdk124.jpg'
+    },
+    {
+        product_name: 'Kệ để đồ',
+        product_price: 12699000,
+        product_image: 'images/KB68002-ke-bep-r01.jpeg'
+    },
+    {
+        product_name: 'Kệ tivi',
+        product_price: 27099000,
+        product_image: 'images/Ke-tivi-ket-hop-ke-trang-tri-2977.jpg'
+    },
+    {
+        product_name: 'Tủ quần áo',
+        product_price: 25999000,
+        product_image: 'images/tu-quan-ao-4-canh-lon-trang-van-go-gp327__3_.jpg'
+    }
+]
