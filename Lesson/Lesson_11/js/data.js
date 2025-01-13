6b6a39d2da73253607f21fb53afb50713cb3dcbe
Quang <quangquan23451822@gmail.com>
2025-01-13 19:27:35 +0700

    minhwang

---
+++ Lesson/Lesson_11/js/data.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+        id: 'SP01',
+        name: 'Giường châu âu',
+        description: 'Size lớn, trắng sữa',
+        price: 8999000,
+        image: 'images/bed.jpg'
+    },
+    {
+        id: 'SP02',
+        name: 'Bàn làm việc',
+        description: 'Size vừa, trắng nâu',
+        price: 3999000,
+        image: 'images/desk.jpg'
+    },
+    {
+        id: 'SP03',
+        name: 'Tủ quần áo',
+        description: '4 ngăn, trắng gỗ',
+        price: 12999000,
+        image: 'images/wardrobe.jpg'
+    },
+    {
+        id: 'SP04',
+        name: 'Kệ để đồ',
+        description: '4 ngăn, trắng gỗ',
+        price: 2499000,
+        image: 'images/drawer.jpg'
+    }
+]
